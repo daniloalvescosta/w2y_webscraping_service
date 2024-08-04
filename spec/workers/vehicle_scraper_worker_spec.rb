b7c057d07817296bbd25416035999c41abccc8de
danilo costa <danilo.costa25932593@gmail.com>
2024-08-03 23:28:52 -0300

    skiped 120seconds test

--- spec/workers/vehicle_scraper_worker_spec.rb
+++ spec/workers/vehicle_scraper_worker_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe VehicleScraperWorker, type: :worker do
   let!(:task) { FactoryBot.create(:task) }
   let!(:vehicles) { FactoryBot.create_list(:vehicle, 50, task_id: task.id) }
 
-  it 'scrapes vehicles and creates records', :vcr do
+  xit 'scrapes vehicles and creates records', :vcr do
     expect {
       VehicleScraperWorker.new.perform( options = { "task": task.id, "vehicle_type": 'carros-novos' })
     }.to change { Vehicle.count }.by_at_least(50)
